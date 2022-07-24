abdf6f7b27800a90f5dba501b7cc9acd622cde39
Zouhir Karim <105131699+Imparoprogrammazione99@users.noreply.github.com>
2022-07-24 20:24:30 +0200

    commit1

---
+++ es-01/exercise-1.js
@@ -0,0 +1,15 @@
+let personName;
+
+function canPlay() {
+  if (true) {
+    personName = 'Paul';
+  }
+
+  personName += ' plays football'; // Argh! personName is not defined
+
+ console.log(personName);
+}
+
+ 
+canPlay();
+
---
+++ es-10/exercise-7.js
@@ -0,0 +1,12 @@
+function sumUntil(maxValue) {
+  let num = 0;
+ 
+  for (let i = 1; i <= maxValue; i++) { 
+    num += i;
+  }
+  return num;
+}
+
+console.log(sumUntil(5));
+
+console.log(sumUntil(5));
